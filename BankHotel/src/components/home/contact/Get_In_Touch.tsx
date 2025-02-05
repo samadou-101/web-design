07873a0492abee2378349a0eaaccc57153a8e265
samadou <mossabm036@gmail.com>
2025-02-05 19:42:44 +0100

    feat: get in touch section completed

---
+++ BankHotel/src/components/home/contact/Get_In_Touch.tsx
@@ -0,0 +1,44 @@
+import style from "../styles/home-contact.module.css";
+import contactPic from "../../../assets/contact-pic.png";
+import contactStar from "../../../assets/contact-star.png";
+import curve from "../../../assets/Curve.png";
+import dropBtn from "../../../assets/drop-btn.png";
+const Get_In_Touch = () => {
+  return (
+    <div className={style["contact-container"]}>
+      <div className={style["phone-num"]}>
+        <h2>+38 032 297 50 20</h2>
+      </div>
+      <div className={style["g-i-t"]}>
+        <div className={style["background"]}>
+          <img src={contactPic} alt="" />
+        </div>
+        <div className={style["wave"]}>
+          <img src={curve} alt="" />
+        </div>
+        <h4>8 LYSTOPADOVOH CHYNU,LVIV</h4>
+        <div className={style["title"]}>
+          <span>
+            GET IN <img src={contactStar} alt="" />
+          </span>
+          <span>TOUCH</span>
+        </div>
+      </div>
+      {/* Progress */}
+      <div className={style["find-room"]}>
+        <span>FIND A ROOM</span>
+        <div className={style["action"]}>
+          <div className={style["check-in"]}>
+            <span>CHECK IN</span> <img src={dropBtn} alt="" />
+          </div>
+          <div className={style["check-out"]}>
+            <span>CHECK OUT</span> <img src={dropBtn} alt="" />
+          </div>
+          <div className={style["book-room"]}>BOOK ROOM</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Get_In_Touch;
--- BankHotel/src/pages/Home.tsx
+++ BankHotel/src/pages/Home.tsx
@@ -1,10 +1,10 @@
-import React from "react";
 import Header from "../components/home/header/Header";
 import Hero from "../components/home/hero/Hero";
 import About from "../components/home/about/About";
 import Services from "../components/home/services/Services";
 import Best_Apartment from "../components/home/apartment/Best_Apartment";
 import Gallery from "../components/home/gallery/Gallery";
+import Get_In_Touch from "../components/home/contact/Get_In_Touch";
 
 function Home() {
   return (
@@ -15,6 +15,7 @@ function Home() {
       <Services />
       <Best_Apartment />
       <Gallery />
+      <Get_In_Touch />
     </>
   );
 }
