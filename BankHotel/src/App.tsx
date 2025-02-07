2ca12c4e46f14df023edf099b797a66da99947c6
samadou <mossabm036@gmail.com>
2025-02-07 16:24:36 +0100

    feat: home/header simple responsiveness

--- BankHotel/src/App.tsx
+++ BankHotel/src/App.tsx
@@ -1,7 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
 import Home from "./pages/Home";
 
 function App() {
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.key === "ArrowDown" || e.key === "ArrowUp" || e.key === " ") {
+        document.documentElement.style.scrollSnapType = "none";
+      } else document.documentElement.style.scrollSnapType = "y proximity"; // Restore it
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
   return (
     <>
       <BrowserRouter>
