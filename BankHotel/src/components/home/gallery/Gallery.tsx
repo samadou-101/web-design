cf0ea375fef5d81fe5906d25750e4ce99fb69d2d
samadou <mossabm036@gmail.com>
2025-02-14 17:01:12 +0100

    feat: home/gallery responsive mobile design

--- BankHotel/src/components/home/gallery/Gallery.tsx
+++ BankHotel/src/components/home/gallery/Gallery.tsx
@@ -6,7 +6,11 @@ import hall from "../../../assets/hall.png";
 import closing from "../../../assets/Polygon1.png";
 import hallSide from "../../../assets/hall-side.png";
 import mirror from "../../../assets/mirror.png";
-
+import mirrorMobile from "../../../assets/mobile-home-gallery-pics/mobile-mirror.png";
+import showerStall from "../../../assets/mobile-home-gallery-pics/shower-stall.png";
+import hallMobile from "../../../assets/mobile-home-gallery-pics/mobile-hall.png";
+import brownRoom from "../../../assets/mobile-home-gallery-pics/mobile-copenhagen.png";
+import apartmentMobile from "../../../assets/mobile-home-gallery-pics/mobile-apartment.png";
 const Gallery = () => {
   return (
     <div className={style["gallery-container"]}>
@@ -34,12 +38,38 @@ const Gallery = () => {
         <div className={style["img2"]}>
           <img src={hall} alt="" />
         </div>
+        <div className={style["mobile-middle-section-pics"]}>
+          <div className={style["main-pic"]}>
+            <img src={apartmentMobile} alt="" />
+          </div>
+          <div className={style["left-section"]}>
+            <div className={style["img-1"]}>
+              <img src={brownRoom} alt="" />
+            </div>
+            <div
+              className={style["img-2"]}
+              style={{ width: "auto", height: "auto" }}
+            >
+              <img src={showerStall} alt="" />
+            </div>
+          </div>
+          <div className={style["right-section"]}>
+            <div className={style["img-1"]}>
+              <img src={mirrorMobile} alt="" />
+            </div>
+            <div
+              className={style["img-2"]}
+              style={{ width: "auto", height: "auto" }}
+            >
+              <img src={hallMobile} alt="" />
+            </div>
+          </div>
+        </div>
         <div className={style["closing"]}>
           <img src={closing} alt="" />
           <p>It is our pleasure to meet your most unrealistic expectations.</p>
         </div>
       </div>
-      {/* Progress */}
       <div className={style["gal-right-section"]}>
         <div className={style["img1"]}>
           <img src={hallSide} alt="" />
