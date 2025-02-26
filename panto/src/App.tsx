d24dfd600e41e018274b15a546583af76a6492a7
samadou <mossabm036@gmail.com>
2025-02-26 20:17:40 +0100

    adding Girloy font

--- panto/src/App.tsx
+++ panto/src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <div className="m-2 bg-red-50 p-2 text-2xl font-bold">test</div>
+      <div className="m-2 bg-red-50 p-2 text-2xl font-extralight">test</div>
     </>
   );
 }
