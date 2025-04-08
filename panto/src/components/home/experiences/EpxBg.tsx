62d1a0f4b350130e06de046f1ef84c4b78db605b
samadou <mossabm036@gmail.com>
2025-04-08 13:17:28 +0100

    style: responsive experiences section

--- panto/src/components/home/experiences/EpxBg.tsx
+++ panto/src/components/home/experiences/EpxBg.tsx
@@ -2,7 +2,7 @@ import expBg from "@assets/experiences/sofa.png";
 
 const EpxBg = () => {
   return (
-    <div className="relative m-8 w-fit md:m-2">
+    <div className="relative m-8 w-fit max-md:min-w-[350px] md:m-2">
       {/* Background squares */}
       <div className="absolute -top-[20%] -left-6 z-0 hidden h-[400px] w-[495px] rounded-lg bg-[#F7F7F7] md:block"></div>
       <div className="absolute top-[50px] -right-[54px] z-4 hidden h-[301px] w-[495px] rounded-lg bg-[#F7F7F7] md:block"></div>
@@ -11,17 +11,17 @@ const EpxBg = () => {
       <img
         src={expBg}
         alt="Background"
-        className="relative z-2 h-auto w-full rounded-lg"
+        className="relative -left-10 z-2 h-auto w-full rounded-lg md:-left-2"
       />
 
       {/* Blur Overlay */}
-      <div className="absolute inset-0 z-3 h-[105%] w-[105%] rounded-lg bg-white/50 backdrop-blur-lg"></div>
+      <div className="absolute inset-0 z-3 h-[108%] w-[105%] rounded-lg bg-white/40 backdrop-blur-lg"></div>
 
       {/* Top Image */}
       <img
         src={expBg}
         alt="Overlayed Image"
-        className="absolute -top-5 left-0 z-5 hidden h-auto w-full rounded-lg md:block"
+        className="absolute -top-2 -left-5 z-6 hidden h-auto w-full rounded-lg md:-top-5 md:left-1"
       />
     </div>
   );
--- panto/src/components/home/experiences/Experiences.tsx
+++ panto/src/components/home/experiences/Experiences.tsx
@@ -3,14 +3,14 @@ import EpxBg from "./EpxBg";
 
 const Experiences = () => {
   return (
-    <div className="mb-20 flex flex-col gap-8 px-4 pt-10 md:mb-60 md:gap-12 md:pt-[10rem] lg:flex-row lg:justify-between lg:gap-0 lg:px-0">
+    <div className="mr-34 mb-20 -ml-8 flex w-full flex-col gap-8 pt-10 max-xl:pr-20 md:mb-60 md:gap-12 md:pt-[10rem] lg:flex-row lg:justify-between lg:gap-2 lg:px-0 2xl:pr-28">
       {/* Background img */}
       <div className="w-full lg:w-auto">
         <EpxBg />
       </div>
       {/* Content */}
-      <div className="flex w-full flex-col gap-5 lg:w-1/3 xl:w-1/4">
-        <span className="text-corange text-lg font-semibold">Experiences</span>
+      <div className="ml-15 flex w-[95%] flex-col gap-5 lg:w-1/2 xl:w-1/3">
+        <span className="text-coragne text-lg font-semibold">Experiences</span>
         <h2 className="text-text text-2xl font-bold md:text-4xl">
           We Provide You The Best Experience
         </h2>
@@ -20,8 +20,8 @@ const Experiences = () => {
           with an elegant and luxurious style and with premium quality materials
         </p>
         <div className="flex items-center gap-2">
-          <span className="text Juli-corange text-base">More Info</span>
-          <span className="text-corange text-sm">
+          <span className="text-coragne text-base">More Info</span>
+          <span className="text-coragne text-sm">
             <HiArrowLongRight size={25} />
           </span>
         </div>
