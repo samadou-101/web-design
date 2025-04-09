d8bbb7a348bae98c1f06a42d2a42c0aea3910aad
samadou <mossabm036@gmail.com>
2025-04-09 09:29:47 +0100

    fix: increasing the padding left for the whyus section

--- panto/src/components/home/why-us/WhyUs.tsx
+++ panto/src/components/home/why-us/WhyUs.tsx
@@ -13,7 +13,7 @@ const WhyUs = () => {
       <h1 className="xl:w[40%] mx-auto py-4 text-center text-4xl font-bold md:text-5xl xl:text-start">
         Why Choosing Us
       </h1>
-      <div className="max-3sm:grid-cols-1 grid grid-cols-3 gap-4 px-4 pt-4 max-md:grid-cols-2 xl:w-[60%]">
+      <div className="max-3sm:grid-cols-1 grid grid-cols-3 gap-4 pt-4 pl-6 max-md:grid-cols-2 xl:w-[60%]">
         <Service title="Luxury facilities" content={luxuryContent1} />
         <Service title="Affordable Price" content={luxuryContent2} />
         <Service title="Many Choices" content={luxuryContent3} />
